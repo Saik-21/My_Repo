b976eab9f8b7aa51f871fdec40e3122f9bb996eb
Saik-21 <skmiriyala21@gmail.com>
2023-10-14 18:52:22 -0500

    closure functions

--- Py-dev.py
+++ Py-dev.py
@@ -1,10 +1,18 @@
-#Stuttering Function
-def stutter(word =''):
-    st = ''
-    for i in range(len(word)):
-        if i<2:
-            st += word[i]
-    return '{}...{}..{}'.format(st,st,word)
-print(stutter(word = 'incredible'))   
+'''def logger(func):
+    def log(*args):
+        print(f"Using {func.__name__} with {args} we get:")
+        print(func(*args))
+    return log
 
+def mul(x,y):
+    return x*y
+
+def sub(x,y):
+    return x-y
+
+mul_log = logger(mul)
+sub_log = logger(sub)
+mul_log(3,3)
+mul_log(7,2)
+sub_log(6,5)''' #This is a  n example of closure
 
