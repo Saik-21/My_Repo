7ff96c55bb45d1ce0867aeefc9e142b6a93a7fbc
Saik-21 <skmiriyala21@gmail.com>
2023-10-15 15:28:40 -0500

    Decorator function

--- Py-dev.py
+++ Py-dev.py
@@ -1,10 +1,29 @@
-#Stuttering Function
-def stutter(word =''):
-    st = ''
-    for i in range(len(word)):
-        if i<2:
-            st += word[i]
-    return '{}...{}..{}'.format(st,st,word)
-print(stutter(word = 'incredible'))   
+#decorator is like a nextension to a function to modigy some changes to a function without causing any affect on the original function.
+#Example
+def exten(func):
+    def wrap(n):
+        lst = []
+        for i in range(1,n+1):
+            if i%2!=0:
+                lst.append(i)
+        print(f"odd list is {lst}")
+        func(n)
+    return wrap
+    
+@exten
+def is_even(n):
+    list = []
+    for i in range(1,n+1):
+        if i%2==0:
+            list.append(i)
+    print(f"Even list is {list}")
+
+@exten
+def is_three(n):
+    list = []
+    for i in range(1,n+1):
+        if i%3==0:
+            list.append(i)
+    print(f"Multiples of 3:{list}")
 
 
