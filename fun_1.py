7fdaf3b34633d223c38f16464d2ebcb0b3d94dfc
Saik-21 <skmiriyala21@gmail.com>
2024-08-19 11:33:39 -0500

    New code functions

--- fun_1.py
+++ fun_1.py
@@ -1,12 +1,19 @@
-class Funct:
-    def __init__(self,n):
-        self.n = n
-    
-    def add(self,t):
-        res = self.n+t
+sqr =  lambda x:x**2
+def check_prime(n):
+   res = True
+   if n==2:
         return res
+   elif n>2:
+       for i in range(2,n):
+           if n%i==0:
+               res = False
+               break
+       return res
+l1 = [i for i in range(1,7)]
+l2 = [i for i in range(1,21)]
 
+#The use of map method is mentioned below
+print(list(map(sqr,l1)))
 
-f1 = Funct(5)
-print(f1.add(3))
-
+#this is the use of filter method
+print(list(filter(check_prime,l2)))
